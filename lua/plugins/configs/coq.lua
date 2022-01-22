d2fcad6839cbde72789df825bfc561084d743f8c
Michel Pupulaire <mpaupulaire4@gmail.com>
2022-01-22 10:57:48 -0500

    more

---
+++ lua/plugins/configs/coq.lua
@@ -0,0 +1,15 @@
+
+vim.opt.completeopt = "menuone,noselect"
+vim.g.coq_settings = {
+  auto_start = true,
+  xdg = true,
+  keymap = {
+    recommended = false
+  },
+  display = {
+    pum = {
+      source_context = {"[", "]"}
+    }
+  }
+}
+
---
+++ lua/plugins/configs/coq_3p.lua
@@ -0,0 +1,5 @@
+require("coq_3p") {
+  { src = "nvimlua", short_name = "VIM", conf_only = true },
+  { src = "bc", short_name = "MATH", precision = 6 },
+}
+
