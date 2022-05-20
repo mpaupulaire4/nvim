ae9fcbe56029cae77774a1b3c8efbe8d59f6afe0
Michel Pupulaire <mpaupulaire4@gmail.com>
2022-05-20 12:42:54 -0400

    save

--- lua/plugins/configs/nvim-cmp.lua
+++ lua/plugins/configs/nvim-cmp.lua
@@ -96,6 +96,9 @@ cmp.setup.filetype('sql', {
 
 -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
 cmp.setup.cmdline('/', {
+  mapping = cmp.mapping.preset.cmdline({
+    -- Your configuration here.
+  }),
   sources = {
     { name = 'buffer' }
   }
