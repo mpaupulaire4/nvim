72e62d9c43e16742a5d783303ac7680421079cb1
Michel Pupulaire <mpaupulaire4@gmail.com>
2022-09-11 11:00:47 -0400

    nullls ignore some projs

--- lua/plugins/configs/nullls.lua
+++ lua/plugins/configs/nullls.lua
@@ -1,13 +1,44 @@
 local builtins = require("null-ls").builtins
 
+local eslint_config_files = {
+    ".eslintrc.js",
+    ".eslintrc.cjs",
+    ".eslintrc.yaml",
+    ".eslintrc.yml",
+    ".eslintrc.json",
+}
+
+local prettier_config_files = {
+    ".prettierrc",
+    ".prettierrc.js",
+    ".prettierrc.cjs",
+    "prettier.config.js",
+    "prettier.config.cjs",
+    ".prettierrc.yaml",
+    ".prettierrc.yml",
+    ".prettierrc.json",
+    ".prettierrc.json5",
+    ".prettierrc.toml",
+}
+
 require("null-ls").setup({
     sources = {
         builtins.diagnostics.eslint_d.with({
             extra_filetypes = { "svelte" },
+            condition = function(utils)
+                return utils.root_has_file(eslint_config_files)
+            end,
         }),
         builtins.code_actions.eslint_d.with({
             extra_filetypes = { "svelte" },
+            condition = function(utils)
+                return utils.root_has_file(eslint_config_files)
+            end,
         }),
-        builtins.formatting.prettierd,
+        builtins.formatting.prettierd.with({
+            condition = function(utils)
+                return utils.root_has_file(prettier_config_files)
+            end,
+        }),
     },
 })
