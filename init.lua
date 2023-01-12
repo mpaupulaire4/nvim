03acf3f72b31e7d32b6bc31b366c828a533c323d
Michel Pupulaire <mpaupulaire4@gmail.com>
2023-01-12 16:38:03 -0500

    spaces

--- init.lua
+++ init.lua
@@ -1,61 +1,61 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 
 vim.g.mapleader = " "
 
 require("lazy").setup("plugins", {
-	ui = {
-		custom_keys = {
-		},
-	},
-	checker = {
-		-- automatically check for plugin updates
-		enabled = true,
-	},
-	change_detection = {
-		-- automatically check for config file changes and reload the ui
-		enabled = true,
-		notify = true, -- get a notification when changes are found
-	},
-	performance = {
-		reset_packpath = true, -- reset the package path to improve startup time
-		rtp = {
-			reset = true, -- reset the runtime path to $VIMRUNTIME and your config directory
-			---@type string[]
-			paths = {}, -- add any custom paths here that you want to indluce in the rtp
-			---@type string[] list any plugins you want to disable here
-			disabled_plugins = {
-				"2html_plugin",
-				"getscript",
-				"getscriptPlugin",
-				"gzip",
-				"logipat",
-				"netrw",
-				"netrwPlugin",
-				"netrwSettings",
-				"netrwFileHandlers",
-				"matchit",
-				"tar",
-				"tarPlugin",
-				"rrhelper",
-				"spellfile_plugin",
-				"vimball",
-				"vimballPlugin",
-				"zip",
-				"zipPlugin",
-			},
-		},
-	},
+  ui = {
+    custom_keys = {
+    },
+  },
+  checker = {
+    -- automatically check for plugin updates
+    enabled = true,
+  },
+  change_detection = {
+    -- automatically check for config file changes and reload the ui
+    enabled = true,
+    notify = true, -- get a notification when changes are found
+  },
+  performance = {
+    reset_packpath = true, -- reset the package path to improve startup time
+    rtp = {
+      reset = true, -- reset the runtime path to $VIMRUNTIME and your config directory
+      ---@type string[]
+      paths = {}, -- add any custom paths here that you want to indluce in the rtp
+      ---@type string[] list any plugins you want to disable here
+      disabled_plugins = {
+        "2html_plugin",
+        "getscript",
+        "getscriptPlugin",
+        "gzip",
+        "logipat",
+        "netrw",
+        "netrwPlugin",
+        "netrwSettings",
+        "netrwFileHandlers",
+        "matchit",
+        "tar",
+        "tarPlugin",
+        "rrhelper",
+        "spellfile_plugin",
+        "vimball",
+        "vimballPlugin",
+        "zip",
+        "zipPlugin",
+      },
+    },
+  },
 })
 require("core.options")
 require("core.mappings")
